1d06aa210662a81c54f5f28982ff38c0f3eef05a
gonicolas <nicolas.gouy@ynov.com>
2024-10-01 10:23:18 +0200

    Routes updated

--- includes/header.php
+++ includes/header.php
@@ -16,16 +16,16 @@ session_start();
 <header>
     <nav>
         <ul>
-            <li><a href="/index.php">Accueil</a></li>
-            <li><a href="/projects.php">Projets</a></li>
-            <li><a href="/contact.php">Contact</a></li>
+            <li><a href="/tp-cv/index.php">Accueil</a></li>
+            <li><a href="/tp-cv/projects.php">Projets</a></li>
+            <li><a href="/tp-cv/contact.php">Contact</a></li>
             <?php if (isset($_SESSION['user_id'])): ?>
-                <li><a href="/cv.php">Mon CV</a></li>
-                <li><a href="/profile.php">Profil</a></li>
-                <li><a href="/logout.php">Déconnexion</a></li>
+                <li><a href="/tp-cv/cv.php">Mon CV</a></li>
+                <li><a href="/tp-cv/profile.php">Profil</a></li>
+                <li><a href="/tp-cv/logout.php">Déconnexion</a></li>
                 <li>Bienvenue, <?php echo $_SESSION['first_name']; ?></li>
             <?php else: ?>
-                <li><a href="/login.php">Connexion</a></li>
+                <li><a href="/tp-cv/login.php">Connexion</a></li>
             <?php endif; ?>
         </ul>
     </nav>
