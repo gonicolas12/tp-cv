75d9ca1dc672054a661f4ec4541ec53db99b09ad
gonicolas <nicolas.gouy@ynov.com>
2024-10-09 11:18:34 +0200

    Header and Footer update

--- includes/footer.php
+++ includes/footer.php
@@ -3,7 +3,7 @@
 <footer>
     <p>&copy; 2024 Mon CV/Portfolio. Tous droits réservés.</p>
     <ul>
-        <li><a href="https://github.com/gonicolas12">GitHub</a></li>
+        <li><a href="https://github.com/gonicolas12" target="_blank">GitHub</a></li>
     </ul>
 </footer>
 
--- includes/header.php
+++ includes/header.php
@@ -27,7 +27,8 @@ if (session_status() === PHP_SESSION_NONE) {
                 <li><a href="/tp-cv/cv.php">Mon CV</a></li>
                 <li><a href="/tp-cv/profile.php">Profil</a></li>
                 <li><a href="/tp-cv/logout.php">Déconnexion</a></li>
-                <li>Bienvenue, <?php echo isset($_SESSION['first_name']) ? $_SESSION['first_name'] : 'Invité'; ?></li>
+                <li>Bienvenue, <?php echo $_SESSION['first_name']; ?></li>
+                <li style="float: right;">Rôle : <?php echo $_SESSION['role']; ?></li>
             <?php else: ?>
                 <li><a href="/tp-cv/login.php">Connexion</a></li>
             <?php endif; ?>
