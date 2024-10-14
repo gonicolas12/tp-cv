b46be9ed12d4baa008ac0484b09f42af35131a89
gonicolas <nicolas.gouy@ynov.com>
2024-10-14 11:16:46 +0200

    CSS Update 3.0

--- index.php
+++ index.php
@@ -1,8 +1,46 @@
 <?php include 'includes/header.php'; ?>
 
 <main>
-    <h1>Bienvenue sur mon portfolio</h1>
-    <p>Découvrez mes projets et mon parcours professionnel.</p>
+    <div class="card">
+        <div class="content">
+            <div class="back">
+                <div class="back-content">
+                    <svg stroke="#ffffff" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" height="50px" width="50px" fill="#ffffff">
+
+                        <g stroke-width="0" id="SVGRepo_bgCarrier"></g>
+
+                        <g stroke-linejoin="round" stroke-linecap="round" id="SVGRepo_tracerCarrier"></g>
+
+                        <g id="SVGRepo_iconCarrier">
+
+
+                        </g>
+
+                    </svg>
+                    <h3>Bienvenue sur mon Portfolio !</h3>
+                </div>
+            </div>
+            <div class="front">
+
+                <div class="img">
+                    <div class="circle">
+                    </div>
+                    <div class="circle" id="right">
+                    </div>
+                    <div class="circle" id="bottom">
+                    </div>
+                </div>
+
+                <div class="front-content">
+                    <div class="description">
+                        <div class="title">
+                        <div class="text-container">
+                        <p>Découvrez mes projets et mon parcours professionnel.</p>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 </main>
 
 <?php include 'includes/footer.php'; ?>
